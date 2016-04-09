bb9e56bb357dbcb497b301513829a7e830f9dc3a
wangkangda <wangkangda@pku.edu.cn>
2016-04-09 10:32:38 +0800

    Add MapActivity

--- Android/Tutu/app/src/main/java/com/hackpku/tutu/MainActivity.java
+++ Android/Tutu/app/src/main/java/com/hackpku/tutu/MainActivity.java
@@ -1,13 +1,28 @@
 package com.hackpku.tutu;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
+
+    private Button mapBtn = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapBtn = (Button) findViewById(R.id.entryMapBtn);
+        mapBtn.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if(v.equals(mapBtn)) {
+            Intent intent = new Intent(this, MapActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- Android/Tutu/app/src/main/java/com/hackpku/tutu/MapActivity.java
+++ Android/Tutu/app/src/main/java/com/hackpku/tutu/MapActivity.java
@@ -15,6 +15,55 @@ public class MapActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
 
+        init();
+    }
+    /**
+     * 初始化AMap对象
+     */
+    private void init() {
+        if (aMap == null) {
+            aMap = mapView.getMap();
+
+        }
+
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 }
