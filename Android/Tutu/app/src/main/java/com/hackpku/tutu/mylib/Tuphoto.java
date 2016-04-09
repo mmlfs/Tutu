b37fe6c0d1bb7d4f3f6f80ac0c9d2c78b71ef010
wangkangda <wangkangda@pku.edu.cn>
2016-04-10 01:12:15 +0800

    change photo when change camera

--- Android/Tutu/app/src/main/java/com/hackpku/tutu/mylib/Tuphoto.java
+++ Android/Tutu/app/src/main/java/com/hackpku/tutu/mylib/Tuphoto.java
@@ -1,6 +1,9 @@
 package com.hackpku.tutu.mylib;
 
+import android.graphics.Bitmap;
+
 import com.amap.api.maps2d.model.BitmapDescriptor;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
 import com.amap.api.maps2d.model.LatLng;
 import com.amap.api.maps2d.model.MarkerOptions;
 
@@ -21,13 +24,15 @@ public class Tuphoto {
     public Tuphoto(double w, double j, BitmapDescriptor photo, String c){
         wei = w;
         jing = j;
-        bitmap = photo;
+        Bitmap bm = photo.getBitmap();
+        zoom = 300f/bm.getWidth();
+        bm = Bitmap.createScaledBitmap(bm, (int)(bm.getWidth()*zoom), (int)(bm.getHeight()*zoom), true);
+        bitmap = BitmapDescriptorFactory.fromBitmap(bm);
         comment = c;
         width = bitmap.getWidth();
         height = bitmap.getHeight();
-        zoom = 300f/width;
     }
     public MarkerOptions getMarker(){
-        return new MarkerOptions().anchor(zoom, zoom).icon(bitmap).position(new LatLng(wei, jing));
+        return new MarkerOptions().anchor(0.5f, 0.5f).icon(bitmap).position(new LatLng(wei, jing));
     }
 }
